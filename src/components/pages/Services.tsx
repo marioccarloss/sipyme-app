a1c3eda700f703d3af4a7f04ac9d61797e2ef1d3
Mario Carlos Roca Peñafiel <mario.roca@dekuple.es>
2023-08-14 23:34:35 +0200

    udpdate

--- src/components/pages/Services.tsx
+++ src/components/pages/Services.tsx
@@ -1,11 +1,6 @@
-import { useLocation } from 'react-router-dom';
 import Dashboard from 'components/templates/Dashboard';
 import ServicesPage from 'components/organisms/services/Show';
 
 export default function Services() {
-  const location = useLocation();
-  if (location.pathname.includes('show')) {
-    return null;
-  }
   return <Dashboard view={<ServicesPage />} />;
 }
